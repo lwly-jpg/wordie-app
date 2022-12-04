1c7733d6a7969d7c5749c1aeaef27ebba384b0ea
adonl <adnlwly@gmail.com>
2022-12-04 14:25:49 +0000

    update grid rows to take past guesses

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,7 +6,7 @@ export default function Grid ( { currentGuess, guesses, turn}) {
   return (
     <div>
       {guesses.map((guess, index) => {
-        return <Row key={index}/>
+        return <Row key={index} guess={guess} />
       })}
     </div>
   )
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export default function Row() {
+export default function Row({ guess }) {
+
+  if (guess) {
+    return (
+      <div className="row past">
+        {guess.map((letter, index) => (
+          <div key={index} className={letter.colour}>{letter.key}</div>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <div className="row">
       <div></div>
